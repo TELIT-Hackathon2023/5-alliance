7fb20c328526a3e3f479a27a3bda7b1be656c3fa
anadTV <80326084+anadTV@users.noreply.github.com>
2023-11-26 06:11:04 +0100

    V3

--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 Papježova družina
 
 Paletu farieb najdete v WebPage/palete.txt
-Pište sem napady kokoti:
+Pište sem napady:
 1 možnosť pridať a zvoliť auto (špz) pre bookovanie
 2
 3
 4
-
-databaza
-https://pma.wedos.net/
-a330221_parking
-Salama123*
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const http = require('http');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const fs = require('fs');
 
 config = JSON.parse(fs.readFileSync("config.json").toString());
@@ -12,12 +12,39 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const server = http.createServer(app);
 
-// // Connect to MySQL Server
-// const conn = mysql.createConnection(config.mysql);
-// conn.connect( (err) => {
-//     if (err) throw err;
-//     console.log("MySQL connected.. database: " + config.mysql.database);
-// });
+app.use("/WebPage", express.static(__dirname + "/WebPage"));
+
+//Connect to MySQL Server
+const conn = mysql.createConnection({
+    host: 'parking-patrik-0978.a.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_hkWeyVpMkWsO2W5KuMo',
+    database: 'defaultdb',
+    port: '20274',
+    // Add this line to specify the authentication plugin
+    authPlugins: {
+      mysql_clear_password: () => () => Buffer.from('AVNS_hkWeyVpMkWsO2W5KuMo'),
+    },
+});
+conn.connect( (err) => {
+    if (err) throw err;
+    console.log("MySQL connected.. database: " + config.mysql.database);
+});
+
+// function for load users to array users = [{},{}]; 
+function loadUsers(){
+    conn.query(`SELECT * FROM users`, (err, result) => {
+        if (err) throw err;
+        //global.users = [];
+        global.users = [];
+        for (i=0; i<result.length; i++) {
+            // set users
+            users.push(result[i]);
+        }
+        console.log(global.users)
+    });
+};
+loadUsers();
 
 // server listen
 server.listen(port, () => {
@@ -30,9 +57,55 @@ app.get('/', (req, res) => {
     res.render('login');
 });
 
+// check login values
 app.post('/', (req, res) => {
-    console.log("ano");
-    res.render('index');
-    // login verification
+    console.log("login check");
+    var username = req.body.username;
+    var password = req.body.password;
+    var founded = false;
+    if(username != "" && password != ""){
+        for(i=0; i<global.users.length; i++){
+            if(username === global.users[i].name && password === global.users[i].password){
+                founded = true;
+                var id = global.users[i].id;
+                res.render('parking', { id : id });
+                break;
+            }
+        }
+        if( !founded ){
+            res.render('login');
+            console.log('wrong pass/name');
+        }
+    }
 });
 
+// profile show
+app.post('/profile', (req, res) => {
+    var id = req.body.profileBtn;
+    console.log(id);
+    var user = global.users[id - 1];
+    res.render("profile", { user: user, id: id });
+});
+
+// edit form in profile
+app.post('/editSpz', (req, res) => {
+    var id = req.body.editBtn;
+    var spz = req.body.spz;
+    conn.query(`UPDATE users SET spz = '${spz}' WHERE id = ${id}`, (err) => {
+        if (err) throw err;
+        loadUsers();
+    });
+    console.log(global.users);
+    
+    setTimeout(() => {
+        var user = global.users[id - 1];
+        res.render('profile', {user: user, id: id});
+    }, 500);
+    
+});
+
+// main page
+app.post('/homeBtn', (req, res) => {
+    var id = req.body.homeBtn;
+    res.render('parking', { id: id });
+});
